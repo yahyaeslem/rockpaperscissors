822ca50366868cd09cbb43f6db82db0389b95337
yahyaeslem <yahyaeslem@gmail.com>
2022-12-16 02:05:41 +0300

    adjust win/lose alert

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const div = document.querySelector('#result');
+const buttons =document.querySelectorAll('button');
+const player= document.querySelector('#Player');
+const computer  = document.querySelector('#Computer');
+let losts = 0;
+let wins = 0;
+let draw = 0;
 // Computer RNG funciton
 console.log("Rock Paper SCissors");
 function getComputerChoice (){
@@ -15,14 +22,6 @@ function getComputerChoice (){
    return i;
 }
 
-// Player choice section
-// function playerSelection(){
-// return prompt("Please type one of these option : Rock , Paper , Scissors").toLowerCase();
-
-
-let losts = 0;
-let wins = 0;
-let draw = 0;
 // Player vs Computer Section
 function singleRoundRPS(userChoice, computerChoice){
  
@@ -71,49 +70,30 @@ function singleRoundRPS(userChoice, computerChoice){
 }
 
 
-//console.log(singleRoundRPS(userChoice, computerChoice));
-// Game Round Count Section
-// function game(){
- 
-//   for (let i = 1; i < 6; i++) {
-//   const userChoice = playerSelection();
-//   const computerChoice = getComputerChoice();
-//   singleRoundRPS(userChoice, computerChoice);
-//   console.log (i +"."+ "ROUND ");
-//   console.log( "Draw = " + draw + " " + "Wins = " + wins +" " + "Losts = " + losts) ;
-
-//  }
- 
-
-// }
-// game();
-const div = document.querySelector('#result');
-const buttons =document.querySelectorAll('button');
-const player= document.querySelector('#Player');
-const computer  = document.querySelector('#Computer');
-
-
-// const rock = document.querySelector('.rock')
-// const paper = document.querySelector('.paper');
-// const scissors = document.querySelector('.scissors');
 buttons.forEach((button) => {
   button.addEventListener('click',()=> {
-    if(wins === 5 || losts === 5){
-      
-      if(wins>losts){
-        div.textContent = "Congratulations!! YOU WON !!"
-      }
-      else if(losts>wins){
-        div.textContent = "Super Intelligent Computer WON"
-      }
-     losts = 0 ;
-     wins = 0;
-    }
+   
     div.textContent =  singleRoundRPS(button.className,getComputerChoice());
     player.textContent = "YOU:" + wins
     computer.textContent = "COMPUTER :" +losts
-    
+    scoreCheck();
   });
   
 });
 
+function scoreCheck(){
+  if(wins === 5 || losts === 5){
+      
+    if(wins>losts){
+      div.textContent = "Congratulations!! YOU WON !!" ;
+      
+    }
+    else if(losts>wins){
+      div.textContent = "Super Intelligent Computer WON";
+      
+    }
+    losts = 0 ;
+    wins = 0;
+   
+  };
+};
